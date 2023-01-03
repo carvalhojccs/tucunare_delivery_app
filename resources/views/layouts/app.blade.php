fa3ddf2ca15db10e4c3566e641dd45e3d30d2928
Jonne C C Silva <jonne.cley@gmail.com>
2023-01-02 22:13:47 -0300

    simple_card component created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,75 +83,59 @@
         <!-- cards -->
         <div class="w-full px-6 py-6 mx-auto">
             <p>Cards</p>
-            <!-- card table -->
-            <div class="flex flex-wrap -mx-3">
-                <div class="flex-none w-full max-w-full px-3">
-                    <div
-                        class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
-                        <div
-                            class="p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                            <h6>Listagem dos planos</h6>
-                        </div>
-                        <div class="flex-auto px-0 pt-0 pb-2">
-                            <div class="p-0 overflow-x-auto">
-                                <!-- table -->
-                                <table class="items-center w-full mb-0 align-top border-gray-200 text-slate-500">
-                                    <thead class="align-bottom">
-                                        <tr>
-                                            <th
-                                                class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                                Nome</th>
-                                            <th
-                                                class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                                URL</th>
-                                            <th
-                                                class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                                Preço</th>
-                                            <th
-                                                class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                                Descriação</th>
-                                            <th
-                                                class="px-6 py-3 font-semibold capitalize align-middle bg-transparent border-b border-gray-200 border-solid shadow-none tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td
-                                                class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                                                <div class="flex px-2 py-1">
-                                                    <p class="mb-0 font-semibold leading-tight text-xs">Plano premium
-                                                    </p>
-                                                </div>
-                                            </td>
-                                            <td
-                                                class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                                                <p class="mb-0 font-semibold leading-tight text-xs">plano-premium</p>
-                                            </td>
-                                            <td
-                                                class="p-2 leading-normal text-center align-middle bg-transparent border-b text-sm whitespace-nowrap shadow-transparent">
-                                                R$100,00
-                                            </td>
-                                            <td
-                                                class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                                                <p class="mb-0 font-semibold leading-tight text-xs">Melhor custo
-                                                    benefício</p>
-                                            </td>
-                                            <td
-                                                class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                                                <a href="javascript:;"
-                                                    class="font-semibold leading-tight text-xs text-slate-400"><i
-                                                        class="mr-2 fas fa-pencil-alt text-slate-700"></i> Edit </a>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                                <!-- end table -->
+            <!-- simple card -->
+
+            <!-- table -->
+            <table class="items-center w-full mb-0 align-top border-gray-200 text-slate-500">
+                <thead class="align-bottom">
+                    <tr>
+                        <th
+                            class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                            Nome</th>
+                        <th
+                            class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                            URL</th>
+                        <th
+                            class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                            Preço</th>
+                        <th
+                            class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                            Descriação</th>
+                        <th
+                            class="px-6 py-3 font-semibold capitalize align-middle bg-transparent border-b border-gray-200 border-solid shadow-none tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+                            <div class="flex px-2 py-1">
+                                <p class="mb-0 font-semibold leading-tight text-xs">Plano premium
+                                </p>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        </td>
+                        <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+                            <p class="mb-0 font-semibold leading-tight text-xs">plano-premium</p>
+                        </td>
+                        <td
+                            class="p-2 leading-normal text-center align-middle bg-transparent border-b text-sm whitespace-nowrap shadow-transparent">
+                            R$100,00
+                        </td>
+                        <td
+                            class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+                            <p class="mb-0 font-semibold leading-tight text-xs">Melhor custo
+                                benefício</p>
+                        </td>
+                        <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+                            <a href="javascript:;" class="font-semibold leading-tight text-xs text-slate-400"><i
+                                    class="mr-2 fas fa-pencil-alt text-slate-700"></i> Edit </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- end table -->
+
+            <!-- end simple card -->
         </div>
     </main>
     @livewireScripts
--- resources/views/livewire/plans/index.blade.php
+++ resources/views/livewire/plans/index.blade.php
@@ -1,35 +1,26 @@
-<div>
+<x-cards.simple_card>
+    <table class="w-full">
+        <thead>
+            <tr
+                class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+                <th class="px-4 py-3">Nome</th>
+                <th class="px-4 py-3">Url</th>
+                <th class="px-4 py-3">Preço</th>
+                <th class="px-4 py-3">Descrição</th>
+                <th class="px-4 py-3">Ação</th>
+            </tr>
+        </thead>
+        <tbody class="bg-white">
+            <tr class="text-gray-700">
+                <td class="px-4 py-3 border">
+                    {{ $name }}
+                </td>
+                <td class="px-4 py-3 text-ms font-semibold border">{{ $url }}</td>
+                <td class="px-4 py-3 text-xs border">{{ $price }}</td>
+                <td class="px-4 py-3 text-sm border">{{ $description }}</td>
+                <td class="px-4 py-3 text-sm border">Edit/Delete</td>
+            </tr>
 
-    <section class="container mx-auto p-6 font-mono">
-        <div class="w-full flex mb-4 p-2 justify-end">
-        </div>
-        <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-            <div class="w-full overflow-x-auto">
-                <table class="w-full">
-                    <thead>
-                        <tr
-                            class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
-                            <th class="px-4 py-3">Nome</th>
-                            <th class="px-4 py-3">Url</th>
-                            <th class="px-4 py-3">Preço</th>
-                            <th class="px-4 py-3">Descrição</th>
-                            <th class="px-4 py-3">Ação</th>
-                        </tr>
-                    </thead>
-                    <tbody class="bg-white">
-                        <tr class="text-gray-700">
-                            <td class="px-4 py-3 border">
-                                {{ $name }}
-                            </td>
-                            <td class="px-4 py-3 text-ms font-semibold border">{{ $url }}</td>
-                            <td class="px-4 py-3 text-xs border">{{ $price }}</td>
-                            <td class="px-4 py-3 text-sm border">{{ $description }}</td>
-                            <td class="px-4 py-3 text-sm border">Edit/Delete</td>
-                        </tr>
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </section>
-</div>
+        </tbody>
+    </table>
+</x-cards.simple_card>
